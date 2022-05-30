3b143f92dc710782fbe954ac623518b8afdd4991
Bilel Taher <bilel.taher01@hotmail.com>
2022-05-30 13:56:56 +0200

    ImageSlider css changes mostly

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,21 +1,21 @@
 export const SliderData = [
   {
-    image: "https://i.ibb.co/tqtQr7z/greensea.png",
-  },
-  {
     image: "https://i.ibb.co/kqPhzGj/Hawksbill.png",
-  },
-  {
-    image: "https://i.ibb.co/CzSJyvV/NGB-Logo-Design-Leatherback.png",
   },
   {
     image: "https://i.ibb.co/52JPj4P/loggerhead.png",
   },
   {
-    image: "https://i.ibb.co/xg5cYPx/kemps-Ridley.png",
+    image: "https://i.ibb.co/CzSJyvV/NGB-Logo-Design-Leatherback.png",
   },
   {
     image: "https://i.ibb.co/DMBtCDX/Olive-Ridley.png",
+  },
+  {
+    image: "https://i.ibb.co/xg5cYPx/kemps-Ridley.png",
+  },
+  {
+    image: "https://i.ibb.co/tqtQr7z/greensea.png",
   },
   {
     image: "https://i.ibb.co/885VbZd/Flatback.png",
