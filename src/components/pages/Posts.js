346358391a249a839f758b35d3ca541ae159bd77
Bilel Taher <bilel.taher01@hotmail.com>
2022-06-10 22:53:27 +0200

    dynamic posts and css added

--- src/components/pages/Posts.js
+++ src/components/pages/Posts.js
@@ -1,12 +1,49 @@
-import React from "react";
-import "../../App.css";
+import { Grid, Pagination } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./Posts.css";
 
 import MUIcards from "../MUIcards";
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [displayed, setDisplayed] = useState([]);
+  const getPosts = async () => {
+    const { data } = await axios.get("http://localhost:5000/post%22");
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    getPosts();
+    if (page === 1) {
+      setDisplayed(posts.slice(0, 9));
+    }
+  }, [posts]);
+
+  useEffect(() => {
+    setDisplayed(posts.slice((page - 1) * 9, page * 9));
+  }, [page]);
   return (
-    <div>
-      <MUIcards></MUIcards>
+    <div className="cards-container">
+      <Grid container spacing={3} alignItems="stretch">
+        {displayed.map((card) => (
+          <Grid item xs={6} sm={4} xl={3} className="card">
+            <MUIcards {...card} />
+          </Grid>
+        ))}
+      </Grid>
+      <div className="pagination">
+        {[...Array(Math.ceil(posts.length / 9)).keys()].map((n) => (
+          <a
+            onClick={() => {
+              setPage(n + 1);
+            }}
+          >
+            {n + 1}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
