5c08566804ece5cabb2afc0c553d5cb1992b8187
Mohamed Rabii CHAKER <chakerrabia9@gmail.com>
2022-06-24 03:43:55 +0100

    added relete report

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,5 +1,9 @@
-import React from "react";
+import { IconButton } from "@mui/material";
+import React, { useContext } from "react";
+import { UserContext } from "../../services/UserContext";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "./Table.css";
+import axios from "axios";
 
 const Table = ({
   data = null,
@@ -11,6 +15,14 @@ const Table = ({
     if (head) return head.toUpperCase();
     return field.toUpperCase();
   };
+  const handleDelete = async (_id) => {
+    await axios
+      .get("http://localhost:5000/rapport/supprimer/" + _id)
+      .then((response) => {
+        window.location.reload(false);
+      });
+  };
+  const userContext = useContext(UserContext);
   return (
     <div>
       <table>
@@ -32,6 +44,15 @@ const Table = ({
                       <a href={row[col.field].split("@")[1]} className="button">
                         Download
                       </a>
+                      {userContext.user && (
+                        <IconButton
+                          color="primary"
+                          aria-label="delete"
+                          component="span"
+                        >
+                          <DeleteIcon onClick={() => handleDelete(row._id)} />
+                        </IconButton>
+                      )}
                     </td>
                   ) : (
                     <td>{row[col.field]}</td>
